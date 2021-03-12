0221facd1d565fd3d31340e4661e38368374f80b
dcouch440 <dcouch440@gmail.com>
2021-03-12 12:12:55 -0800

    add route for home and project creeation

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'pg'
-require 'pry'
-also_reload('lib/**/*.rb')
+[
+  'sinatra',
+  'sinatra/reloader',
+  'pg',
+  'pry',
+  './lib/project',
+  './lib/volunteer',
+  './db'
 
-get '/' do
-  'welcome'
+].each { |data| (require data) }
+
+also_reload 'lib/**/*.rb'
+
+
+get ('/') do
+  erb :projects_new
+end
+
+get ('/projects') do
+  @projects = Projects.all()
+end
+
+post ('/projects') do
+  project = Project.new({:id => nil, :title => params[:title]})
+  project.save()
+  redirect to "/projects/#{project.id}"
+end
+
+get ('/projects/:id') do
+  @project = Project.find(params[:id])
+  erb :project
 end
