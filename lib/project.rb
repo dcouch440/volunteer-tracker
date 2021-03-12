7b8bcc87784a996ec4c41e7915b80c61aeaf6d86
dcouch440 <dcouch440@gmail.com>
2021-03-12 10:21:41 -0800

    add passing methods for .delete | .find | .update

--- lib/project.rb
+++ lib/project.rb
@@ -16,16 +16,32 @@ class Project
     self.title == compare_project.title
   end
 
-  def self.all
+  def self.all()
     projects = []
     all_projects = DB.exec("SELECT * FROM projects")
     
     all_projects.each do |project|
       title = project['title']
-      id = project['project']
+      id = project['project'].to_i()
       projects << Project.new({:title => title, :id => id})
     end
-
+  
     projects
   end
+
+  def self.find(id)
+    found = DB.exec("select * FROM projects WHERE id = #{id}").first()
+    id , title = found.values_at("id", "title")
+    Project.new({:id => (id.to_i), :title => title})
+  end
+
+  def update(attr)
+    @title = attr.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id}")
+  end
+
+  def delete 
+    DB.exec("DELETE FROM projects WHERE id = #{@id}")
+  end
+
 end
